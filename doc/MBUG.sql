badb346dd9e38e11fa63da7f9454987a6130771a
Fc-404 <33905772+Fc-404@users.noreply.github.com>
2021-04-05 23:00:22 +0800

    20210405

---
+++ doc/MBUG.sql
@@ -0,0 +1,82 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : root
+ Source Server Type    : MySQL
+ Source Server Version : 50726
+ Source Host           : localhost:3306
+ Source Schema         : mbug
+
+ Target Server Type    : MySQL
+ Target Server Version : 50726
+ File Encoding         : 65001
+
+ Date: 05/04/2021 10:18:36
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for BUGS
+-- ----------------------------
+DROP TABLE IF EXISTS `BUGS`;
+CREATE TABLE `BUGS`  (
+  `bugid` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'bug主键',
+  `discoverer` bigint(20) NOT NULL COMMENT '发现者id',
+  `project` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '项目名',
+  `environment` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '环境',
+  `description` varchar(400) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '描述',
+  `probability` int(3) NULL DEFAULT NULL COMMENT '概率',
+  `time` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '时间',
+  PRIMARY KEY (`bugid`) USING BTREE,
+  INDEX `发现者id`(`discoverer`) USING BTREE,
+  INDEX `项目`(`project`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for DEBUGS
+-- ----------------------------
+DROP TABLE IF EXISTS `DEBUGS`;
+CREATE TABLE `DEBUGS`  (
+  `bugid` bigint(20) NOT NULL COMMENT '和BUG外键约束',
+  `debuger` bigint(20) NOT NULL COMMENT '调试者id',
+  `method` varchar(1024) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '解决方法',
+  `time` datetime NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '时间',
+  PRIMARY KEY (`bugid`) USING BTREE,
+  INDEX `调试者id`(`debuger`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for ENVIRONMENT
+-- ----------------------------
+DROP TABLE IF EXISTS `ENVIRONMENT`;
+CREATE TABLE `ENVIRONMENT`  (
+  `name` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '环境cell',
+  PRIMARY KEY (`name`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for PROJECT
+-- ----------------------------
+DROP TABLE IF EXISTS `PROJECT`;
+CREATE TABLE `PROJECT`  (
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '项目名',
+  PRIMARY KEY (`name`) USING BTREE
+) ENGINE = MyISAM CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for USER
+-- ----------------------------
+DROP TABLE IF EXISTS `USER`;
+CREATE TABLE `USER`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键递增',
+  `name` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '姓名',
+  `pswd` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  `department` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '部门',
+  `job` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '工作',
+  `mail` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '邮箱',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = MyISAM AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
